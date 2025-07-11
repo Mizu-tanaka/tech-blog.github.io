1dbe71e3ec774124de153d6cc868e0369fe2087d
Mizu-tanaka <mizuki.tanaka0602@acsl.ics.keio.ac.jp>
2025-07-12 01:41:49 +0900

    setup ogp

--- setting.ts
+++ setting.ts
@@ -1,2 +1,2 @@
 // ex) export const TARGET_URL = "https://xxxx.github.io/tech-blog.github.io/";
-export const TARGET_URL = "https://";
+export const TARGET_URL = "https://mizu-tanaka.github.io/tech-blog.github.io/";
